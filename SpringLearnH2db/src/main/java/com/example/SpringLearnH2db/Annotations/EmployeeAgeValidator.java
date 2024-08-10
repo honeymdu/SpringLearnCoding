c902edccd016491c1826d1a37c92a0330199b151
Himanshu Mehra <97497671+honeymdu@users.noreply.github.com>
2024-08-10 10:51:25 +0530

    Creation Custom Annotation for Custom Validation

--- SpringLearnH2db/src/main/java/com/example/SpringLearnH2db/Annotations/EmployeeAgeValidator.java
+++ SpringLearnH2db/src/main/java/com/example/SpringLearnH2db/Annotations/EmployeeAgeValidator.java
@@ -7,10 +7,23 @@ public class EmployeeAgeValidator implements ConstraintValidator <EmployeeAgeVal
 
     @Override
     public boolean isValid(Integer age, ConstraintValidatorContext context) {
-        if(age%2==0)
-        return true;
-        else 
+        
+        if(age==1)
         return false;
+
+        if(age<=3)
+        return true;
+
+        if(age%2==0||age%3==0)
+        return false;
+
+        for(int i =2; i<=Math.sqrt(age);i++){
+            if(age%i==0){
+                return false;
+            }
+        }
+        return true;
+    
     }
 
 }
