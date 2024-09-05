aad73e6982f8c21c6a4c01e6f1b469d0091b1cec
Himanshu Mehra <97497671+honeymdu@users.noreply.github.com>
2024-09-05 16:22:10 +0530

    JWT generate Token and Varification

---
+++ SpringLearnH2db/src/main/java/com/example/SpringLearnH2db/Services/JwtService.java
@@ -0,0 +1,47 @@
+package com.example.SpringLearnH2db.Services;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.Set;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.stereotype.Service;
+
+import com.example.SpringLearnH2db.Entitys.User;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+@Service
+public class JwtService {
+
+    private String JwtSecretKey;
+
+    private SecretKey getSecretKey() {
+        return Keys.hmacShaKeyFor(JwtSecretKey.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String GenerateToken(User user) {
+        return Jwts.builder()
+                .subject(user.getId().toString())
+                .claim("UserEmail", user.getEmail())
+                .claim("Roles", Set.of("ADMIN", "USER"))
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60))
+                .signWith(getSecretKey())
+                .compact();
+
+    }
+
+    public Long getUserIdFromToken(String token) {
+        Claims claims = Jwts.parser()
+                .verifyWith(getSecretKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+        return Long.valueOf(claims.getSubject());
+    }
+
+}
